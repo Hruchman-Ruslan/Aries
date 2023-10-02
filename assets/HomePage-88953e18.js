f3b1de7fef71c07de83241e40a66b01c79932f20
Hruchman-Ruslan <Hruchman-Ruslan@users.noreply.github.com>
2023-10-02 13:38:44 +0000

    deploy: 2a0fcbbd3df2043656d8a43ad12bd539c0257d1a

---
+++ assets/HomePage-88953e18.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-327b6206.js";const r=()=>e.jsx("div",{children:e.jsx("p",{children:"Home"})});function t(){return e.jsx(e.Fragment,{children:e.jsx(r,{})})}export{t as default};
---
+++ assets/ProgramPage-ef8f9fe1.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-327b6206.js";const e=()=>r.jsx("div",{children:r.jsx("p",{children:"Program"})});function t(){return r.jsx(r.Fragment,{children:r.jsx(e,{})})}export{t as default};
