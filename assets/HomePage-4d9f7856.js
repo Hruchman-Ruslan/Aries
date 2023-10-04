f646309f8a31f5f3592b5705d4f54fb938a8e2cc
Hruchman-Ruslan <Hruchman-Ruslan@users.noreply.github.com>
2023-10-04 17:31:20 +0000

    deploy: 6e9405ca4a6201ae6aceb3f5445269736415a161

---
+++ assets/HomePage-4d9f7856.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-8fe3ee66.js";const r=()=>e.jsx("div",{children:e.jsx("p",{children:"Home"})});function t(){return e.jsx(e.Fragment,{children:e.jsx(r,{})})}export{t as default};
