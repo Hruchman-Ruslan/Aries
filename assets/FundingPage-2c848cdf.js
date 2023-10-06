4233aa60caa0859e7f8f846f3508a66b54415347
Hruchman-Ruslan <Hruchman-Ruslan@users.noreply.github.com>
2023-10-06 18:48:59 +0000

    deploy: 9c58f42cb45c83a5fa3f65e9322570924a27fafc

--- assets/FundingPage-2c848cdf.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-30e87320.js";const r=()=>n.jsx("div",{children:n.jsx("p",{children:"Funding"})});function i(){return n.jsx(n.Fragment,{children:n.jsx(r,{})})}export{i as default};
