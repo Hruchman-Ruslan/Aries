e1f2ca4081516f5cb8287feff5c8767b65bde3a6
Ruslan <Giovingo92@gmail.com>
2023-10-09 20:50:42 +0300

    feat: add hero content

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,15 @@
+import styles from "../Home/Home.module.css";
+import scrollArrow from "../../images/scroll_arrow.svg";
+
 export const Home = () => {
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <>
+      <h1 className={styles.main__title}>
+        We Are Here For Your{" "}
+        <span className={styles.main__span}>Artificial Intelligence</span>{" "}
+        Startup.
+      </h1>
+      <img src={scrollArrow} alt="scrollArrow" className={styles.main_svg} />
+    </>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import { Home } from "../components/Home/Home";
+import styles from "../styles/Section.module.css";
 
 export default function HomePage() {
   return (
     <>
-      <Home />
+      <section className={styles.main__section}>
+        <Home />
+      </section>
     </>
   );
 }
