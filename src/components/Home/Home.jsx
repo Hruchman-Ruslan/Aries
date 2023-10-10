9ca987c7b6dd78fcdc1ee43ce7642e9a09dbed19
Ruslan <Giovingo92@gmail.com>
2023-10-10 21:15:28 +0300

    feat: add bg images

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,18 @@
 import styles from "../Home/Home.module.css";
+import styleSection from "../../styles/Section.module.css";
+
 import scrollArrow from "../../images/scroll_arrow.svg";
 
 export const Home = () => {
   return (
-    <>
+    <section className={styleSection.main__section}>
+      <div className={styles.main_small} />
       <h1 className={styles.main__title}>
         We Are Here For Your{" "}
         <span className={styles.main__span}>Artificial Intelligence</span>{" "}
         Startup.
       </h1>
       <img src={scrollArrow} alt="scrollArrow" className={styles.main_svg} />
-    </>
+    </section>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,9 @@
 import { Home } from "../components/Home/Home";
-import styles from "../styles/Section.module.css";
 
 export default function HomePage() {
   return (
     <>
-      <section className={styles.main__section}>
-        <Home />
-      </section>
+      <Home />
     </>
   );
 }
