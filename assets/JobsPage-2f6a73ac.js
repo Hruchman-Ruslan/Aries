7864dbf772ecbc78a1d7469f9de376ee4f8829e0
Hruchman-Ruslan <Hruchman-Ruslan@users.noreply.github.com>
2023-10-14 18:20:20 +0000

    deploy: b4f4b5694649b79ca8792732ef910c3a42e5497e

--- assets/JobsPage-2f6a73ac.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-34bda36f.js";const s=()=>r.jsx("div",{children:r.jsx("p",{children:"Jobs"})});function n(){return r.jsx(r.Fragment,{children:r.jsx(s,{})})}export{n as default};
--- assets/ProgramPage-850ae781.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-34bda36f.js";const e=()=>r.jsx("div",{children:r.jsx("p",{children:"Program"})});function t(){return r.jsx(r.Fragment,{children:r.jsx(e,{})})}export{t as default};
